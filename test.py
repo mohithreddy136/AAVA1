5f73d5ba3576e584cdeb47892d38884d4ffc92d3
mohithreddy136 <145823338+mohithreddy136@users.noreply.github.com>
2025-12-22 10:35:17 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
-print("hello world")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import TimeoutException
+from webdriver_manager.chrome import ChromeDriverManager
+
+# -------------------------------
+# Headless Chrome for CI
+# -------------------------------
+options = Options()
+options.add_argument("--headless=new")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--window-size=1920,1080")
+
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=options
+)
+driver.get("https://www.google.com")
+print(f"title:{driver.current_url}")
